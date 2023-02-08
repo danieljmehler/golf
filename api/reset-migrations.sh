fb472d31147fbecadd73250c99221671962b700a
Daniel Mehler <danieljmehler@gmail.com>
2023-02-07 19:11:43 -0500

    Update README

--- api/reset-migrations.sh
+++ api/reset-migrations.sh
@@ -2,4 +2,3 @@ rm -f db.sqlite3
 rm -rf {base,course,golfer,holeinfo,holescore,round,tee}/migrations/*
 touch {base,course,golfer,holeinfo,holescore,round,tee}/migrations/__init__.py
 python manage.py makemigrations
-
